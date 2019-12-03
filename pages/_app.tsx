6e68e2aa6d8f8225dde7120035bda1c75fed07d6
sheldar <kewin.polok@gmail.com>
2019-12-03 22:16:49 +0100

    Chtulhu green theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React from 'react';
 import App from 'next/app';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
@@ -13,10 +15,18 @@ class CustomApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
+    const theme = createMuiTheme({
+      palette: {
+        primary: { main: '#2fa200' },
+      },
+    });
+
     return (
       <>
         <CssBaseline />
-        <Component {...pageProps} />;
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />;
+        </ThemeProvider>
       </>
     );
   }
