903bae425326866a8915380deeb2f29bbe269d43
sheldar <kewin.polok@gmail.com>
2019-12-03 21:43:49 +0100

    Update Home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 
-const Home = (): JSX.Element => (
+const Home: React.FunctionComponent = () => (
   <div>
     <Head>
       <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
+      <link href="/favicon.ico" rel="icon" />
     </Head>
     <h1>nextjs-project-template</h1>
   </div>
