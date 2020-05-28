f526bcb0c67e65b9dd7007354a9666c453c881a3
sheldar <kewin.polok@gmail.com>
2020-05-28 19:30:15 +0200

    Update .eslintrc

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -3,7 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-const NavBar = () => {
+const NavBar: React.FunctionComponent = () => {
   return (
     <AppBar position="static">
       <Toolbar>
